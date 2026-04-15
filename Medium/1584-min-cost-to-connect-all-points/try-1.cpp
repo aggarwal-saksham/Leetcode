32e9bb2a7f77ac10894297a7583f6c897c5110c4
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 19:30:45 +0530

    Add Medium/1584-min-cost-to-connect-all-points/try-1.cpp

---
+++ Medium/1584-min-cost-to-connect-all-points/try-1.cpp
@@ -0,0 +1,93 @@
+/*
+ * Problem #1584: Min Cost to Connect All Points
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 6/22/2025, 1:02:23 AM
+ * Link: https://leetcode.com/problems/min-cost-to-connect-all-points/
+ */
+
+
+class DisjointSet {
+    vector<int> rank, parent, size;
+
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        } else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        } else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        } else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        DisjointSet ds(n);
+        vector<pair<int, pair<int, int>>> nedges;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int dist = abs(points[i][0] - points[j][0]) +
+                           abs(points[i][1] - points[j][1]);
+                nedges.push_back({dist, {i, j}});
+            }
+        }
+
+        sort(nedges.begin(), nedges.end());
+
+        int ans = 0;
+        for (auto it : nedges) {
+            int wt = it.first;
+            int u = it.second.first;
+            int v = it.second.second;
+
+            if (ds.findUPar(u) != ds.findUPar(v)) {
+                ans += wt;
+                ds.unionBySize(u, v);
+            }
+        }
+
+        return ans;
+    }
+};
