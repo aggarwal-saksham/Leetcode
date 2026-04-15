84f572ee8ae13a19c1f18cd40f1e989f8f047ff3
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 19:17:29 +0530

    Add Medium/143-reorder-list/try-2.cpp

---
+++ Medium/143-reorder-list/try-2.cpp
@@ -0,0 +1,63 @@
+/*
+ * Problem #143: Reorder List
+ * Difficulty: Medium
+ * Submission: Try 2
+ * status: Accepted
+ * Language: cpp
+ * Date: 1/12/2026, 7:15:48 PM
+ * Link: https://leetcode.com/problems/reorder-list/
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if (!head || !head->next || !head->next->next) return;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* middle = slow;
+        ListNode* end = fast;
+        ListNode* next2mid = slow->next;
+        slow->next = nullptr;
+
+        ListNode* temp = next2mid;
+        ListNode* prev = NULL;
+        ListNode* front = NULL;
+//1>2>3>     <4<5<6<7
+//1 7 2 6 3 5 4    
+        while(temp != NULL){
+            front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+
+        }
+        end = prev;
+        temp = head;
+        ListNode* tempend = NULL;
+        while(temp != NULL && end != NULL){
+            front = temp->next;
+            tempend = end->next;
+            temp->next = end;
+            end->next = front;
+            temp = front;
+            end = tempend;
+        }
+    
+        
+    }
+};
