ffbfc878712da5fc66c9d7c2c615f7ac29143b1d
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 20:11:34 +0530

    Add Medium/235-lowest-common-ancestor-of-a-binary-search-tree/try-2.cpp

---
+++ Medium/235-lowest-common-ancestor-of-a-binary-search-tree/try-2.cpp
@@ -0,0 +1,40 @@
+/*
+ * Problem #235: Lowest Common Ancestor of a Binary Search Tree
+ * Difficulty: Medium
+ * Submission: Try 2
+ * status: Accepted
+ * Language: cpp
+ * Date: 12/19/2024, 12:11:36 AM
+ * Link: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ */
+
+/**
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *};
+ */
+
+class Solution
+{
+    public:
+        TreeNode* lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+        {
+            while (root)
+            {
+                if (p->val > root->val && q->val > root->val)
+                {
+                    root = root->right;
+                }
+                else if (p->val < root->val && q->val < root->val)
+                {
+                    root = root->left;
+                }
+                else return root;
+            }
+            return root;
+        }
+};
