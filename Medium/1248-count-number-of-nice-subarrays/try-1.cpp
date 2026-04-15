12a26e75f5995b9f546e5e0295f75dfedb9cdb3f
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 19:27:48 +0530

    Add Medium/1248-count-number-of-nice-subarrays/try-1.cpp

---
+++ Medium/1248-count-number-of-nice-subarrays/try-1.cpp
@@ -0,0 +1,37 @@
+/*
+ * Problem #1248: Count Number of Nice Subarrays
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 6/29/2025, 12:30:37 AM
+ * Link: https://leetcode.com/problems/count-number-of-nice-subarrays/
+ */
+
+class Solution {
+public:
+    // first solve lc 930 binary subarray
+    int func(vector<int>& nums, int k) {
+        int n = nums.size();
+        int cnt = 0;
+        int odds = 0;
+        int l = 0, r = 0;
+
+        while(r < n){
+            if((nums[r] & 1) == 1) odds++;
+
+            while(odds > k){
+                if((nums[l] & 1) == 1) odds--; 
+                l++;
+            }
+
+            cnt += r - l + 1;
+            r++;
+        }
+        return cnt;
+
+    }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return {func(nums, k) - func(nums, k - 1)};
+    }
+};
