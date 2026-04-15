852c7d50118fcd4300b675409164f24743feb90a
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 20:08:43 +0530

    Add Medium/973-k-closest-points-to-origin/try-1.cpp

---
+++ Medium/973-k-closest-points-to-origin/try-1.cpp
@@ -0,0 +1,33 @@
+/*
+ * Problem #973: K Closest Points to Origin
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 1/23/2025, 3:34:22 PM
+ * Link: https://leetcode.com/problems/k-closest-points-to-origin/
+ */
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<double, int>> maxH;
+        for(int i = 0; i < points.size(); i++){
+            maxH.push({sqrt(pow(points[i][0],2) + pow(points[i][1],2)), i});
+            if(maxH.size() > k){
+                maxH.pop();
+            }
+
+        }
+
+        vector<vector<int>> ans;
+
+        while(!maxH.empty()){
+            int ind = maxH.top().second;
+            ans.push_back(points[ind]);
+            maxH.pop();
+        }
+
+        return ans;        
+    }
+};
