e44c94f0f10941bb0369f9044e0c837553a6e516
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 19:14:26 +0530

    Add Medium/538-convert-bst-to-greater-tree/try-1.cpp

---
+++ Medium/538-convert-bst-to-greater-tree/try-1.cpp
@@ -0,0 +1,42 @@
+/*
+ * Problem #538: Convert BST to Greater Tree
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 3/22/2026, 12:41:06 AM
+ * Link: https://leetcode.com/problems/convert-bst-to-greater-tree/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void bstToGstHelper(TreeNode* root, int& nodeSum) {
+        // If root is null, make no changes.
+        if (!root) {
+            return;
+        }
+
+        bstToGstHelper(root->right, nodeSum);
+        nodeSum += root->val;
+        // Update the value of root.
+        root->val = nodeSum;
+        bstToGstHelper(root->left, nodeSum);
+    }
+    
+    TreeNode* convertBST(TreeNode* root) {
+        int nodeSum = 0;
+        bstToGstHelper(root, nodeSum);
+        return root;
+    }
+};
