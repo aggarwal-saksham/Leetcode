32c1a769cdc80ca429df23c6517428ec274920d4
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 19:16:26 +0530

    Add Medium/3824-minimum-k-to-reduce-array-within-limit/try-3.cpp

---
+++ Medium/3824-minimum-k-to-reduce-array-within-limit/try-3.cpp
@@ -0,0 +1,35 @@
+/*
+ * Problem #3824: Minimum K to Reduce Array Within Limit
+ * Difficulty: Medium
+ * Submission: Try 3
+ * status: Accepted
+ * Language: cpp
+ * Date: 1/31/2026, 8:25:00 PM
+ * Link: https://leetcode.com/problems/minimum-k-to-reduce-array-within-limit/
+ */
+
+class Solution {
+public:
+    int minimumK(vector<int>& nums) {
+        long long l = 1, h = 1e5;
+        int ans = 0;
+        while(l <= h){
+            long long mid = (l + h) / 2;
+            long long total = 0;
+
+            for(int x : nums){
+                total += (x + mid - 1) / mid;
+                
+            }
+
+            if(total <= mid * mid){
+                ans = mid;
+                h = mid - 1;
+            }
+            else l = mid + 1;
+            
+        }
+
+        return ans;
+    }
+};
