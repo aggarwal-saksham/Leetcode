579c7205f281971d4526275803199f88a6fc1492
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 19:15:25 +0530

    Add Medium/1823-find-the-winner-of-the-circular-game/try-1.cpp

---
+++ Medium/1823-find-the-winner-of-the-circular-game/try-1.cpp
@@ -0,0 +1,17 @@
+/*
+ * Problem #1823: Find the Winner of the Circular Game
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 2/23/2026, 8:23:40 PM
+ * Link: https://leetcode.com/problems/find-the-winner-of-the-circular-game/
+ */
+
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        if(n == 1) return 1;
+        return ((findTheWinner(n - 1, k) + (k - 1)) % n) + 1;
+    }
+};
