9d0bfc58ccbb7fafc66b0bb236c8e4443e28e696
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 20:03:45 +0530

    Add Medium/122-best-time-to-buy-and-sell-stock-ii/try-1.cpp

---
+++ Medium/122-best-time-to-buy-and-sell-stock-ii/try-1.cpp
@@ -0,0 +1,33 @@
+/*
+ * Problem #122: Best Time to Buy and Sell Stock II
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 6/1/2025, 11:44:57 PM
+ * Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+ */
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+
+        for (int buy = 0; buy < 2; buy++) {
+            dp[n][buy] = 0;
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int buy = 0; buy < 2; buy++) {
+                if (buy) {
+                    dp[i][buy] = max(-prices[i] + dp[i + 1][0], dp[i + 1][1]);
+                } 
+                else {
+                    dp[i][buy] = max(prices[i] + dp[i + 1][1], dp[i + 1][0]);
+                }
+            }
+        }
+        return dp[0][1];
+    }
+};
