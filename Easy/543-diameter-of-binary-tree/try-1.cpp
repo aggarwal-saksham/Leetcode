8628674cd8c95f2440098553eaef2e626c843394
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 20:13:40 +0530

    Add Easy/543-diameter-of-binary-tree/try-1.cpp

---
+++ Easy/543-diameter-of-binary-tree/try-1.cpp
@@ -0,0 +1,40 @@
+/*
+ * Problem #543: Diameter of Binary Tree
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 10/4/2024, 3:19:22 PM
+ * Link: https://leetcode.com/problems/diameter-of-binary-tree/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root, int &diameter) {
+        if(!root) return 0;
+
+        int lh = maxDepth(root->left,diameter);
+        int rh = maxDepth(root->right, diameter);
+        diameter = (max(diameter,lh + rh));
+
+        return 1 + max(lh,rh);
+        
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+       int diameter = 0;
+       maxDepth(root, diameter);
+       return diameter;
+        
+    }
+};
