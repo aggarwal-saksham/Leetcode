0f50528cd587530fe55dc445b00f9b6878dd94cb
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 20:16:00 +0530

    Add Easy/680-valid-palindrome-ii/try-1.cpp

---
+++ Easy/680-valid-palindrome-ii/try-1.cpp
@@ -0,0 +1,46 @@
+/*
+ * Problem #680: Valid Palindrome II
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 9/11/2024, 12:46:32 AM
+ * Link: https://leetcode.com/problems/valid-palindrome-ii/
+ */
+
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        //deddde
+        //ebcbb e cecabbacec bbcbe
+        //ebcbbcecabbacecbbcbe
+        //ababaab
+        int left = 0;
+        int right = s.length() - 1;
+        while(left < right){
+            if(s[left] != s[right]){
+                int l1 = left + 1, r1 = right;
+                int l2 = left, r2 = right - 1;
+
+                while(l1 < r1  && s[l1] == s[r1]){
+                    
+                    l1++;
+                    r1--;
+                }
+                while(l2 < r2  && s[l2] == s[r2]){
+                    l2++;
+                    r2--;
+                }
+                return l1 >= r1  || l2 >= r2;
+
+
+            }
+
+            else{
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+};
