6d536ddb91acba5f79d78149cf33394e1caf655b
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 19:18:26 +0530

    Add Medium/150-evaluate-reverse-polish-notation/try-2.cpp

---
+++ Medium/150-evaluate-reverse-polish-notation/try-2.cpp
@@ -0,0 +1,56 @@
+/*
+ * Problem #150: Evaluate Reverse Polish Notation
+ * Difficulty: Medium
+ * Submission: Try 2
+ * status: Accepted
+ * Language: cpp
+ * Date: 1/3/2026, 3:21:08 PM
+ * Link: https://leetcode.com/problems/evaluate-reverse-polish-notation/
+ */
+
+class Solution
+{
+    public:
+        int evalRPN(vector<string> &exp)
+        {
+            stack<string> st;
+            int i = 0;
+            int n = exp.size();
+            while (i < n)
+            {
+                if (exp[i] != "+" && exp[i] != "-" && exp[i] != "*" && exp[i] != "/")
+            {
+                    st.push(exp[i]);
+                }
+                else
+                {
+                    int t1 = stoi(st.top());
+                    st.pop();
+                    int t2 = stoi(st.top());
+                    st.pop();
+                    int ans = 0;
+                    if (exp[i] == "/")
+                    {
+                        ans = t2 / t1;
+                    }
+                    else if (exp[i] == "*")
+                    {
+                        ans = t2 * t1;
+                    }
+                    else if (exp[i] == "+")
+                    {
+                        ans = t2 + t1;
+                    }
+                    else if (exp[i] == "-")
+                    {
+                        ans = t2 - t1;
+                    }
+
+                    st.push(to_string(ans));
+                }
+                i++;
+            }
+
+            return stoi(st.top());
+        }
+};
