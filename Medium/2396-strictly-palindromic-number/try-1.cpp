1fc3aed68b5730b904ef969d29f7bd7996bc36c4
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 19:21:15 +0530

    Add Medium/2396-strictly-palindromic-number/try-1.cpp

---
+++ Medium/2396-strictly-palindromic-number/try-1.cpp
@@ -0,0 +1,16 @@
+/*
+ * Problem #2396: Strictly Palindromic Number
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 8/29/2025, 1:44:26 PM
+ * Link: https://leetcode.com/problems/strictly-palindromic-number/
+ */
+
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+     return false;   
+    }
+};
