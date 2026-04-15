d1d6e80585ae9e02f641fdc7dc21402c9ff56641
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 20:18:01 +0530

    Add Medium/15-3sum/try-1.cpp

---
+++ Medium/15-3sum/try-1.cpp
@@ -0,0 +1,47 @@
+/*
+ * Problem #15: 3Sum
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 8/2/2024, 1:18:05 AM
+ * Link: https://leetcode.com/problems/3sum/
+ */
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        set<vector<int>> ans;
+        int sum=0;
+        for(int i=0;i<nums.size()-2;i++){
+            
+            int j=i+1;
+            int k=nums.size()-1;
+
+            
+            while(j<k){
+            sum=nums[i]+nums[j]+nums[k];
+                if(sum<0){
+                    j++;
+                }
+                else if(sum>0){
+                    k--;
+                }
+                else{
+                    ans.insert({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+                }
+            }
+        }
+
+        vector<vector<int>> ansmain;
+
+        for(auto it : ans){
+         ansmain.push_back(it);
+        }
+
+        return ansmain;
+    }
+};
