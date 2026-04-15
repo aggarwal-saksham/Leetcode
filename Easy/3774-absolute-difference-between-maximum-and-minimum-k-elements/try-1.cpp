6976f84c20fe9f5df27884a4c40772085a864c67
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 19:19:45 +0530

    Add Easy/3774-absolute-difference-between-maximum-and-minimum-k-elements/try-1.cpp

---
+++ Easy/3774-absolute-difference-between-maximum-and-minimum-k-elements/try-1.cpp
@@ -0,0 +1,22 @@
+/*
+ * Problem #3774: Absolute Difference Between Maximum and Minimum K Elements
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 12/19/2025, 12:19:21 AM
+ * Link: https://leetcode.com/problems/absolute-difference-between-maximum-and-minimum-k-elements/
+ */
+
+class Solution {
+public:
+    int absDifference(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int mini = 0, maxi = 0, n = nums.size();
+        for(int l = 0; l < k; l++){
+            mini += nums[l];
+            maxi += nums[ n - l - 1];
+        }
+        return (maxi - mini);
+    }
+};
