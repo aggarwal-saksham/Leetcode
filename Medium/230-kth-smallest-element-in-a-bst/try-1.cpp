b8ac6d60f0ea5c72944cf871a2561780d7658a68
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 20:11:50 +0530

    Add Medium/230-kth-smallest-element-in-a-bst/try-1.cpp

---
+++ Medium/230-kth-smallest-element-in-a-bst/try-1.cpp
@@ -0,0 +1,39 @@
+/*
+ * Problem #230: Kth Smallest Element in a BST
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 12/17/2024, 5:48:31 PM
+ * Link: https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+ */
+
+/**
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
+ */
+class Solution
+{
+    public:
+        void inorder(TreeNode *root, vector<int> &arr)
+        {
+            if (!root) return;
+            inorder(root->left, arr);
+            arr.push_back(root->val);
+            inorder(root->right, arr);
+        }
+        int kthSmallest(TreeNode *root, int k)
+        {
+            vector<int> arr;
+            inorder(root, arr);
+            //int n= arr.size();
+            return arr[k - 1];
+        }
+};
