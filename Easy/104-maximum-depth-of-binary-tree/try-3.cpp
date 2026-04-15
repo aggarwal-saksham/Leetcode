ba500244313cb5ac45eecc49a553cce80ec7f7c0
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 19:24:12 +0530

    Add Easy/104-maximum-depth-of-binary-tree/try-3.cpp

---
+++ Easy/104-maximum-depth-of-binary-tree/try-3.cpp
@@ -0,0 +1,52 @@
+/*
+ * Problem #104: Maximum Depth of Binary Tree
+ * Difficulty: Easy
+ * Submission: Try 3
+ * status: Accepted
+ * Language: cpp
+ * Date: 7/16/2025, 2:10:20 AM
+ * Link: https://leetcode.com/problems/maximum-depth-of-binary-tree/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if (root == NULL)
+            return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int size = q.size();
+            vector<int> level;
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                if (node->left)
+                    q.push(node->left);
+                if (node->right)
+                    q.push(node->right);
+                level.push_back(node->val);
+            }
+
+            ans.push_back(level);
+        }
+        return ans;
+    }
+
+    int maxDepth(TreeNode* root) {
+        vector<vector<int>> ans = levelOrder(root);
+        return ans.size();
+    }
+};
