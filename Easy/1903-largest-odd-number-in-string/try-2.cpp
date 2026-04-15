8bc22b8f19917c1c12afa62248a57e4dc4632134
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 20:17:32 +0530

    Add Easy/1903-largest-odd-number-in-string/try-2.cpp

---
+++ Easy/1903-largest-odd-number-in-string/try-2.cpp
@@ -0,0 +1,37 @@
+/*
+ * Problem #1903: Largest Odd Number in String
+ * Difficulty: Easy
+ * Submission: Try 2
+ * status: Accepted
+ * Language: cpp
+ * Date: 8/16/2024, 1:04:03 AM
+ * Link: https://leetcode.com/problems/largest-odd-number-in-string/
+ */
+
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        int n = num.size();
+        string result;
+        int index = -1;
+        // if (int(num[n - 1]) % 2 == 1) {
+        //     result = num;
+        // }
+        for (int i = n - 1; i >= 0; i--) {
+            if (int(num[i]) % 2 == 1) {
+                index = i;
+                break;
+            }
+            //  else
+            //     index = -1;
+        }
+
+        if (index == -1) {
+
+            result = ' ';
+        }
+        result = num.substr(0, index + 1);
+
+        return result;
+    }
+};
