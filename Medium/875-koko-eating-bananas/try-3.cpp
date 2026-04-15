dded83d70f3bdad1ed2c17fa51fca919a25dd7be
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 19:18:35 +0530

    Add Medium/875-koko-eating-bananas/try-3.cpp

---
+++ Medium/875-koko-eating-bananas/try-3.cpp
@@ -0,0 +1,45 @@
+/*
+ * Problem #875: Koko Eating Bananas
+ * Difficulty: Medium
+ * Submission: Try 3
+ * status: Accepted
+ * Language: cpp
+ * Date: 1/2/2026, 6:59:25 PM
+ * Link: https://leetcode.com/problems/koko-eating-bananas/
+ */
+
+class Solution {
+public:
+    int find_max_pile(vector<int>& piles) {
+        int max_pile = INT_MIN;
+
+        int n = piles.size();
+        for (int i = 0; i < n; i++) {
+            max_pile = max(max_pile, piles[i]);
+        }
+        return max_pile;
+    }
+
+    long long calc_total_hrs(vector<int>& piles, int k) {
+        int n = piles.size();
+        long long total_hrs = 0;
+        for (int i = 0; i < n; i++) {
+            total_hrs += (piles[i] + k - 1LL) / k;
+        }
+        return total_hrs;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        long long sum = accumulate(piles.begin(), piles.end(), 0LL);
+int low = (sum + h - 1) / h,
+            high = find_max_pile(piles);
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            long long total_hrs = calc_total_hrs(piles, mid);
+            if (total_hrs <= h) {
+                high = mid - 1;
+            } else
+                low = mid + 1;
+        }
+        return low;
+    }
+};
