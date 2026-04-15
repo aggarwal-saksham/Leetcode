aaaeeffd3615db4e34bda14415fd3a37705a7d23
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 19:16:47 +0530

    Add Easy/2549-count-distinct-numbers-on-board/try-1.cpp

---
+++ Easy/2549-count-distinct-numbers-on-board/try-1.cpp
@@ -0,0 +1,17 @@
+/*
+ * Problem #2549: Count Distinct Numbers on Board
+ * Difficulty: Easy
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 1/22/2026, 8:04:28 PM
+ * Link: https://leetcode.com/problems/count-distinct-numbers-on-board/
+ */
+
+class Solution {
+public:
+    int distinctIntegers(int n) {
+        if(n ==1 ) return 1;
+        return n -1;
+    }
+};
