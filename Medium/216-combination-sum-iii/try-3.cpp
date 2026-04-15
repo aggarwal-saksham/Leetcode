65353a4708de94befe2109cec41a4e270cb3e15c
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 19:15:19 +0530

    Add Medium/216-combination-sum-iii/try-3.cpp

---
+++ Medium/216-combination-sum-iii/try-3.cpp
@@ -0,0 +1,40 @@
+/*
+ * Problem #216: Combination Sum III
+ * Difficulty: Medium
+ * Submission: Try 3
+ * status: Accepted
+ * Language: cpp
+ * Date: 2/24/2026, 12:59:31 AM
+ * Link: https://leetcode.com/problems/combination-sum-iii/
+ */
+
+class Solution {
+public:
+    void findComb(vector<vector<int>> &ans, vector<int> &comb, int k, int n){
+        if(comb.size() == k && n == 0){
+            ans.push_back(comb);
+            return;
+        }
+        if(comb.size() == 0){
+            for(int i = 1; i <= 9; i++){
+                comb.push_back(i);
+                findComb(ans, comb, k, n-i);
+                comb.pop_back();
+            }
+        }
+        if(comb.size() > 0 && comb.size() < k){
+            for(int i = comb.back() + 1; i <= 9; i++){
+                comb.push_back(i);
+                findComb(ans, comb, k, n-i);
+                comb.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> combinationSum3(int k, int n)  {
+        vector<vector<int>> ans;
+        vector<int> comb;
+        if(k > n) return ans;
+        findComb(ans, comb, k, n);
+        return ans;
+    }
+};
