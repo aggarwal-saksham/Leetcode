18420c717d5052bb743d144162d611aa6a4a9bd5
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 19:19:38 +0530

    Add Medium/3-longest-substring-without-repeating-characters/try-4.cpp

---
+++ Medium/3-longest-substring-without-repeating-characters/try-4.cpp
@@ -0,0 +1,36 @@
+/*
+ * Problem #3: Longest Substring Without Repeating Characters
+ * Difficulty: Medium
+ * Submission: Try 4
+ * status: Accepted
+ * Language: cpp
+ * Date: 12/19/2025, 10:37:33 PM
+ * Link: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ */
+
+class Solution
+{
+    public:
+        int lengthOfLongestSubstring(string s)
+        {
+            int n = s.size();
+            int l = 0, r = 0;
+            unordered_set<int> st;
+            int maxi = 0;
+            while (l <= r && r < n)
+            {
+                if (st.find(s[r]) == st.end())
+                {
+                    st.insert(s[r]);
+                    maxi = max(maxi, r - l + 1);
+                    r++;
+                }
+                else
+                {
+                    st.erase(s[l]);
+                    l++;
+                }
+            }
+            return maxi;
+        }
+};
