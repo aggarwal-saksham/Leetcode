810fe0e6f39f54448276820e1e846515b9ed88ea
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 20:19:44 +0530

    Add Medium/31-next-permutation/try-2.cpp

---
+++ Medium/31-next-permutation/try-2.cpp
@@ -0,0 +1,42 @@
+/*
+ * Problem #31: Next Permutation
+ * Difficulty: Medium
+ * Submission: Try 2
+ * status: Accepted
+ * Language: cpp
+ * Date: 7/10/2024, 9:55:01 PM
+ * Link: https://leetcode.com/problems/next-permutation/
+ */
+
+class Solution
+{
+    public:
+        void nextPermutation(vector<int> &nums)
+        {
+            int n = nums.size();
+            int index = -1;
+            for (int i = n - 2; i >= 0; i--)
+            {
+                if (nums[i] < nums[i + 1])
+                {
+                    index = i;
+                    break;
+                }
+            }
+            if (index == -1)
+            {
+                reverse(nums.begin(), nums.end());
+                return;
+            }
+
+            for (int i = n - 1; i > index ; i--)
+            {
+                if (nums[i] > nums[index])
+                {
+                    swap(nums[i], nums[index]);
+                    break;
+                }
+            }
+            reverse(nums.begin() + index + 1, nums.end());
+        }
+};
