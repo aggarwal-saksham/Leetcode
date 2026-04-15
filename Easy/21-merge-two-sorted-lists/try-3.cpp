05dccc991a13f5b8cd44ed298f781a8bfe631698
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 19:29:16 +0530

    Add Easy/21-merge-two-sorted-lists/try-3.cpp

---
+++ Easy/21-merge-two-sorted-lists/try-3.cpp
@@ -0,0 +1,56 @@
+/*
+ * Problem #21: Merge Two Sorted Lists
+ * Difficulty: Easy
+ * Submission: Try 3
+ * status: Accepted
+ * Language: cpp
+ * Date: 6/24/2025, 7:34:52 PM
+ * Link: https://leetcode.com/problems/merge-two-sorted-lists/
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* head1, ListNode* head2) {
+        if (!head1 && !head2)
+            return NULL;
+        if (!head1)
+            return head2;
+        if (!head2)
+            return head1;
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+        ListNode* t1 = head1;
+        ListNode* t2 = head2;
+        while(t1 && t2){
+            if(t1->val < t2->val){
+                temp->next = t1;
+                t1 = t1->next;
+            }
+            else{
+                temp->next = t2;
+                t2 = t2->next;
+            }
+
+            temp = temp->next;
+        }
+
+        if(t1){
+            temp->next = t1;            
+        }
+        if(t2){
+            temp->next = t2;           
+        }
+        
+        return dummy->next;
+    }
+};
