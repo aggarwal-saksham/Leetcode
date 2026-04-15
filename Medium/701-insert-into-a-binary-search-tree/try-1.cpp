315d89bcd3b95112b5985ff42d64cd60539dc049
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 20:12:41 +0530

    Add Medium/701-insert-into-a-binary-search-tree/try-1.cpp

---
+++ Medium/701-insert-into-a-binary-search-tree/try-1.cpp
@@ -0,0 +1,47 @@
+/*
+ * Problem #701: Insert into a Binary Search Tree
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 10/30/2024, 5:12:41 PM
+ * Link: https://leetcode.com/problems/insert-into-a-binary-search-tree/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        TreeNode* newN = new TreeNode(val);
+        if(!root) return newN;
+        TreeNode* temp = root;
+        while(temp){
+            if(temp->val < val){
+                if(temp->right) temp = temp->right;
+                else {
+                    temp->right = newN;
+                    break;
+                }
+            }
+            else {
+                if(temp->left) temp = temp->left;
+                else{
+                    temp->left = newN;
+                    break;
+                }
+            }
+        }
+
+        return root;
+    }
+};
