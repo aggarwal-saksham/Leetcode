92509807bceebb07a36c5dce2a4a45d1b8efaee7
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 19:17:50 +0530

    Add Medium/678-valid-parenthesis-string/try-4.cpp

---
+++ Medium/678-valid-parenthesis-string/try-4.cpp
@@ -0,0 +1,40 @@
+/*
+ * Problem #678: Valid Parenthesis String
+ * Difficulty: Medium
+ * Submission: Try 4
+ * status: Accepted
+ * Language: cpp
+ * Date: 1/8/2026, 4:34:06 PM
+ * Link: https://leetcode.com/problems/valid-parenthesis-string/
+ */
+
+class Solution {
+public:
+    bool checkValidString(string s) {
+        stack<int> leftSt;
+        stack<int> starSt;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '(') leftSt.push(i);
+            else if(s[i] == '*') starSt.push(i);
+            else{
+                if(!leftSt.empty()) leftSt.pop();
+                else if(!starSt.empty()) starSt.pop();
+                else return false;
+            }
+
+        }
+        while(!leftSt.empty()){
+            if(starSt.empty()) return false;
+            if(starSt.top() > leftSt.top()){
+                starSt.pop();
+                leftSt.pop();
+            }
+            else return false;
+        }
+
+        // return (leftSt.empty());
+        return (true);
+
+        
+    }
+};
