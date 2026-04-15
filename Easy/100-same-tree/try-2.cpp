a3943ccfa507aee3b8406ec76a3e32136262c9ed
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 19:23:52 +0530

    Add Easy/100-same-tree/try-2.cpp

---
+++ Easy/100-same-tree/try-2.cpp
@@ -0,0 +1,35 @@
+/*
+ * Problem #100: Same Tree
+ * Difficulty: Easy
+ * Submission: Try 2
+ * status: Accepted
+ * Language: cpp
+ * Date: 7/18/2025, 2:50:09 PM
+ * Link: https://leetcode.com/problems/same-tree/
+ */
+
+/**
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ *right(right) {}
+ *};
+ */
+class Solution {
+public:
+    bool check(TreeNode* p, TreeNode* q) {
+        if (!p && !q)
+            return true;
+        if ((p && !q) || (!p && q))
+            return false;
+        if (p->val != q->val)
+            return false;
+        return (check(p->left, q->left) && check(p->right, q->right));
+    }
+    bool isSameTree(TreeNode* p, TreeNode* q) { return check(p, q); }
+};
