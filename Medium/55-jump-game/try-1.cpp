0ce723d68f88e80da01f122a5883200c866c0932
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 20:10:55 +0530

    Add Medium/55-jump-game/try-1.cpp

---
+++ Medium/55-jump-game/try-1.cpp
@@ -0,0 +1,23 @@
+/*
+ * Problem #55: Jump Game
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 1/1/2025, 12:41:41 AM
+ * Link: https://leetcode.com/problems/jump-game/
+ */
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxInd = 0;
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            if(i > maxInd) return false;
+            maxInd = max(maxInd, nums[i] + i);
+            if(maxInd >= n-1) return true;
+        }
+        return false;
+    }
+};
