178f51a933a74b057280ca9c831a719d3f966c57
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 19:18:50 +0530

    Add Easy/1-two-sum/try-5.cpp

---
+++ Easy/1-two-sum/try-5.cpp
@@ -0,0 +1,27 @@
+/*
+ * Problem #1: Two Sum
+ * Difficulty: Easy
+ * Submission: Try 5
+ * status: Accepted
+ * Language: cpp
+ * Date: 12/28/2025, 11:41:14 PM
+ * Link: https://leetcode.com/problems/two-sum/
+ */
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mpp;
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            mpp[nums[i]] = i;
+
+        }
+        for(int i = 0; i < n; i++){
+            if(mpp.find(target - nums[i]) != mpp.end() && i != mpp[target - nums[i]]){
+                return {i, mpp[target - nums[i]]};
+            }
+        }
+        return {0, 0};
+    }
+};
