aa52df696224e2f88613d416b3e5a3914c98d4b3
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 20:19:59 +0530

    Add Medium/53-maximum-subarray/try-1.cpp

---
+++ Medium/53-maximum-subarray/try-1.cpp
@@ -0,0 +1,34 @@
+/*
+ * Problem #53: Maximum Subarray
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 7/10/2024, 2:24:15 PM
+ * Link: https://leetcode.com/problems/maximum-subarray/
+ */
+
+class Solution
+{
+    public:
+        int maxSubArray(vector<int> &nums)
+        {
+            int n = nums.size();
+            int sum = 0;
+            int max = INT_MIN;
+            for (int i = 0; i < n; i++)
+            {
+                sum += nums[i];
+
+                if (sum > max){
+                    max = sum;
+                }
+
+                if(sum < 0){
+                    sum = 0;
+                }
+
+            }
+            return max;
+        }
+};
