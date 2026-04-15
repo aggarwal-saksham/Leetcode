eb2067d44c54c8b026081f2621e136f6d606c29f
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 19:28:31 +0530

    Add Easy/225-implement-stack-using-queues/try-3.cpp

---
+++ Easy/225-implement-stack-using-queues/try-3.cpp
@@ -0,0 +1,53 @@
+/*
+ * Problem #225: Implement Stack using Queues
+ * Difficulty: Easy
+ * Submission: Try 3
+ * status: Accepted
+ * Language: cpp
+ * Date: 6/27/2025, 1:56:59 AM
+ * Link: https://leetcode.com/problems/implement-stack-using-queues/
+ */
+
+class MyStack
+{
+    public:
+    queue<int> q1;
+    MyStack() {}
+
+    void push(int x)
+    {
+        int n = q1.size();
+        q1.push(x);
+        for(int i = 0; i < n; i++){
+            q1.push(q1.front());
+            q1.pop();
+        }
+    }
+
+    int pop()
+    {
+
+        int x = q1.front();
+        q1.pop();
+        return x;
+    }
+
+    int top()
+    {
+        return q1.front();
+    }
+
+    bool empty()
+    {
+        return (q1.empty());
+    }
+};
+
+/**
+ *Your MyStack object will be instantiated and called as such:
+ *MyStack* obj = new MyStack();
+ *obj->push(x);
+ *int param_2 = obj->pop();
+ *int param_3 = obj->top();
+ *bool param_4 = obj->empty();
+ */
