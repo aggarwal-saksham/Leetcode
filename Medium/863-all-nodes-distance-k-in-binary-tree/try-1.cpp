d0557b23286d06b79dbac79712708616494839a2
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-15 19:23:18 +0530

    Add Medium/863-all-nodes-distance-k-in-binary-tree/try-1.cpp

---
+++ Medium/863-all-nodes-distance-k-in-binary-tree/try-1.cpp
@@ -0,0 +1,67 @@
+/*
+ * Problem #863: All Nodes Distance K in Binary Tree
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 7/26/2025, 1:57:02 AM
+ * Link: https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/
+ */
+
+class Solution {
+public:
+    void solve(TreeNode* node, TreeNode* parent, map<TreeNode*, TreeNode*>& m,
+               map<TreeNode*, bool>& n) {
+        if (!node)
+            return;
+
+        n[node] = false;
+        if (parent)
+            m[node] = parent;
+        solve(node->left, node, m, n);
+        solve(node->right, node, m, n);
+    }
+
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+
+        vector<int> ans;
+        if (k == 0)
+            return {target->val};
+
+        map<TreeNode*, TreeNode*> parent;
+        map<TreeNode*, bool> visited;
+        solve(root, NULL, parent, visited);
+
+        queue<TreeNode*> q;
+        q.push(target);
+        int dist = 0;
+        while (!q.empty()) {
+            dist++;
+            int sz = q.size();
+            for (int i = 0; i < sz; ++i) {
+                TreeNode* temp = q.front();
+                q.pop();
+                visited[temp] = true;
+
+                if (temp->left && !visited[temp->left]) {
+                    q.push(temp->left);
+                    if (dist == k)
+                        ans.push_back(temp->left->val);
+                }
+                if (temp->right && !visited[temp->right]) {
+                    q.push(temp->right);
+                    if (dist == k)
+                        ans.push_back(temp->right->val);
+                }
+                if (parent[temp] && !visited[parent[temp]]) {
+                    q.push(parent[temp]);
+                    if (dist == k)
+                        ans.push_back(parent[temp]->val);
+                }
+            }
+            if (dist == k)
+                break;
+        }
+        return ans;
+    }
+};
