e5281ccfdf7c7d12539cca6a25a2c89b67b392af
Saksham Aggarwal <saksham080305@gmail.com>
2026-05-28 02:10:24 +0530

    Add Hard/1707-maximum-xor-with-an-element-from-array/try-1.cpp

---
+++ Hard/1707-maximum-xor-with-an-element-from-array/try-1.cpp
@@ -0,0 +1,92 @@
+/*
+ * Problem #1707: Maximum XOR With an Element From Array
+ * Difficulty: Hard
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 5/28/2026, 1:58:54 AM
+ * Link: https://leetcode.com/problems/maximum-xor-with-an-element-from-array/
+ */
+
+struct Node{
+    Node *links[2];
+
+    bool containsKey(int bit){
+        return links[bit] != nullptr;
+    }
+
+    void put(int bit, Node* node){
+        links[bit] = node;
+    }
+    Node* get(int bit){
+        return links[bit];
+    }
+    
+};
+class Solution {
+public:
+    Node* root = new Node();
+
+    void insert(int num){
+        Node *node = root;
+        for(int i = 31; i>= 0; i--){
+            int bit = (num >> i) & 1;
+            if(!node->containsKey(bit)){
+                node->put(bit, new Node());
+            }
+            node = node->get(bit);
+        }
+    }
+
+    int getMaxXOR(int num){
+        Node* node = root;
+        int ans = 0;
+        for(int i = 31; i >= 0; i--){
+            int bit = (num >> i) & 1;
+
+            if(node->containsKey(1 - bit)){
+                ans = ans | (1 << i);
+                node = node->get(1 - bit);
+            }
+            else node = node->get(bit);
+        }
+        return ans;
+    }
+
+    static bool cmp(vector<int>& a, vector<int>& b){
+        return a[1] < b[1];
+    }
+
+    vector<int> maximizeXor(vector<int>& nums, vector<vector<int>>& queries) {
+        vector<vector<int>> copy = queries;
+        for(int i = 0; i < copy.size(); i++){
+            copy[i].push_back(i);
+        }
+
+        sort(copy.begin(), copy.end(), cmp);
+        sort(nums.begin(), nums.end());
+        int l = 0;
+        int n = nums.size();
+        vector<int> ans(queries.size());
+        for(auto q : copy){
+            int x = q[0];
+            int m = q[1];
+            int og = q[2];
+
+            while(l < n && nums[l] <= m){
+                insert(nums[l]);
+                l++;
+            } 
+
+            if(l == 0){
+                ans[og] = -1;
+            }
+            else{
+                ans[og] = getMaxXOR(x);
+            }
+        }
+
+
+        return ans;
+    }
+};
