69b9aa6fa97b0d39f3fab1087ba619f246ebeb3a
Saksham Aggarwal <saksham080305@gmail.com>
2026-05-28 02:11:52 +0530

    Add Medium/1922-count-good-numbers/try-1.cpp

---
+++ Medium/1922-count-good-numbers/try-1.cpp
@@ -0,0 +1,30 @@
+/*
+ * Problem #1922: Count Good Numbers
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 5/14/2026, 12:43:20 AM
+ * Link: https://leetcode.com/problems/count-good-numbers/
+ */
+
+class Solution {
+public:
+    int M = 1e9 + 7;
+    long long findpow(long long x, long long n) {
+        if (n == 0) {
+            return 1;
+        }
+        long long ans = findpow(x, n / 2);
+        ans *= ans;
+        ans %= M;
+        if (n % 2 == 1) {
+            ans *= x;
+            ans %= M;
+        }
+        return ans;
+    }
+    int countGoodNumbers(long long n) {
+        return (long long)findpow(5, (n + 1) / 2) * findpow(4, n / 2) % M;
+    }
+};
