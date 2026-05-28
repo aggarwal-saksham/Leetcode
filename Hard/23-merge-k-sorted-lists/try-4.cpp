aa3e0bd9d3ff1ba094a725eb85add399d1c68dad
Saksham Aggarwal <saksham080305@gmail.com>
2026-05-29 00:09:40 +0530

    Add Hard/23-merge-k-sorted-lists/try-4.cpp

---
+++ Hard/23-merge-k-sorted-lists/try-4.cpp
@@ -0,0 +1,50 @@
+/*
+ * Problem #23: Merge k Sorted Lists
+ * Difficulty: Hard
+ * Submission: Try 4
+ * status: Accepted
+ * Language: cpp
+ * Date: 5/28/2026, 11:59:12 PM
+ * Link: https://leetcode.com/problems/merge-k-sorted-lists/
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int k = lists.size();
+        typedef pair<int, ListNode*> pin;
+        priority_queue<pin, vector<pin>, greater<pin>> minH;
+
+        for (int i = 0; i < k; i++) {
+            ListNode* head = lists[i];
+            if (head)
+                minH.push({head->val, head});
+        }
+
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+        while (!minH.empty()) {
+            auto it = minH.top();
+            minH.pop();
+
+            temp->next = it.second;
+            temp = temp->next;
+            
+            if (it.second->next) {
+                minH.push({it.second->next->val, it.second->next});
+            }
+        }
+
+        return dummy->next;
+    }
+};
