208ffc7a4ed060d681e7cb20897343920b9394f1
Saksham Aggarwal <saksham080305@gmail.com>
2026-05-30 20:23:06 +0530

    Add Medium/57-insert-interval/try-2.cpp

---
+++ Medium/57-insert-interval/try-2.cpp
@@ -0,0 +1,41 @@
+/*
+ * Problem #57: Insert Interval
+ * Difficulty: Medium
+ * Submission: Try 2
+ * status: Accepted
+ * Language: cpp
+ * Date: 5/30/2026, 8:22:47 PM
+ * Link: https://leetcode.com/problems/insert-interval/
+ */
+
+class Solution
+{
+    public:
+        vector<vector < int>> insert(vector<vector < int>> &intervals, vector< int > &newInterval)
+        {
+            vector<vector<int>> ans;
+            int n = intervals.size();
+            //if (n == 0) return ans.push_back(newInterval);
+            int i = 0;
+            while (i < n && intervals[i][1] < newInterval[0])
+            {
+                ans.push_back(intervals[i]);
+                i++;
+            }
+
+            while (i < n && intervals[i][0] <= newInterval[1])
+            {
+                newInterval[0] = min(intervals[i][0], newInterval[0]);
+                newInterval[1] = max(intervals[i][1], newInterval[1]);
+                i++;
+            }
+            ans.push_back(newInterval);
+
+            while (i < n)
+            {
+                ans.push_back(intervals[i]);
+                i++;
+            }
+            return ans;
+        }
+};
