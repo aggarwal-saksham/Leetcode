3b6ff80d421f31c94f2a747cccc5c397e45a1afe
Saksham Aggarwal <saksham080305@gmail.com>
2026-05-30 14:33:50 +0530

    Add Medium/355-design-twitter/try-1.cpp

---
+++ Medium/355-design-twitter/try-1.cpp
@@ -0,0 +1,62 @@
+/*
+ * Problem #355: Design Twitter
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 5/29/2026, 5:21:34 PM
+ * Link: https://leetcode.com/problems/design-twitter/
+ */
+
+class Twitter {
+public:
+    unordered_map<int, set<int>> following;
+    unordered_map<int, vector<pair<int, int>>> tweets;
+    int clock = 0;
+    Twitter() {
+        
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        tweets[userId].push_back({clock, tweetId});
+        clock++;
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        priority_queue<pair<int, int>> pq;
+        vector<int> feed;
+        for(auto u : following[userId]){
+            for(auto t : tweets[u]){
+                pq.push({t});
+            }
+        }
+        for(auto t : tweets[userId]){
+            pq.push({t});
+        }
+        while(!pq.empty() && feed.size() < 10){
+            feed.push_back(pq.top().second);
+            pq.pop();
+        }
+        return feed;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        following[followerId].insert(followeeId);
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        if(following[followerId].find(followeeId) != following[followerId].end()){
+            following[followerId].erase(followeeId);
+        }
+
+    }
+};
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter* obj = new Twitter();
+ * obj->postTweet(userId,tweetId);
+ * vector<int> param_2 = obj->getNewsFeed(userId);
+ * obj->follow(followerId,followeeId);
+ * obj->unfollow(followerId,followeeId);
+ */
