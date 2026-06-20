df7801ba33af0d8514af7d6fdd6323df2938b433
Saksham Aggarwal <saksham080305@gmail.com>
2026-06-21 00:03:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    vector<int> dijkstra(int n, vector<vector<int>> &edges, int src) {
+        // Code here
+        vector<vector<pair<int, int>>> adj(n);
+        for(auto &e : edges){
+            adj[e[0]].push_back({e[1], e[2]});
+            adj[e[1]].push_back({e[0], e[2]});
+        }
+        
+        vector<int> dist(n, 1e9);
+        typedef pair<int, int> pii;
+        priority_queue<pii, vector<pii>, greater<>> pq;
+        
+        dist[src] = 0;
+        pq.push({0, src});
+        
+        while(!pq.empty()){
+            auto [d, node] = pq.top();
+            pq.pop();
+            
+            for(auto it : adj[node]){
+                int nbr = it.first;
+                int wt = it.second;
+                
+                if(dist[node] + wt < dist[nbr]){
+                    dist[nbr] = dist[node] + wt;
+                    pq.push({dist[nbr], nbr});
+                }
+            }
+        }
+        return dist;
+    }
+};
