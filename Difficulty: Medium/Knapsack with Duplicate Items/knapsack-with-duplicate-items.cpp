fbe1de2df4922c06a3660b83c989527b0eb9cfc9
Saksham Aggarwal <saksham080305@gmail.com>
2026-06-29 00:44:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int rec(int W, vector<int> &val, vector<int> &wt, int i, vector<vector<int>> &dp) {
+        
+        if(W < 0) return -1e9;
+        if(i < 0) return 0;
+        if(dp[i][W] != -1) return dp[i][W];
+        
+        return dp[i][W] = max(val[i] + rec(W-wt[i], val, wt, i, dp),
+            rec(W, val, wt, i-1, dp));
+    }
+    int knapSack(vector<int>& val, vector<int>& wt, int W) {
+        // code here
+        int n = wt.size();
+        vector<vector<int>> dp(n + 1, vector<int>(W + 1, -1));
+        return rec(W, val, wt, n - 1, dp);
+    }
+};
