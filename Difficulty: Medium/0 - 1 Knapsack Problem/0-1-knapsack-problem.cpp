3ca377a352bc7d075dc849e767ea5298deffa76a
Saksham Aggarwal <saksham080305@gmail.com>
2026-06-29 00:47:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -4,15 +4,16 @@ class Solution {
     // Function to return max value that can be put in knapsack of capacity.
     int rec(int W, vector<int> &val, vector<int> &wt, int i, vector<vector<int>> &dp) {
         
-        if(W < 0) return -1e9;
         if(i < 0) return 0;
         if(dp[i][W] != -1) return dp[i][W];
         
-        return dp[i][W] = max(val[i] + rec(W-wt[i], val, wt, i-1, dp),
-            rec(W, val, wt, i-1, dp));
+        int take = -1e9;
+        if(wt[i] <= W)
+            take = val[i] + rec(W - wt[i], val, wt, i - 1, dp);
         
+        int notTake = rec(W, val, wt, i - 1, dp);
         
-       
+        return dp[i][W] = max(take, notTake);
     }
     int knapsack(int W, vector<int> &val, vector<int> &wt) {
         // code here
