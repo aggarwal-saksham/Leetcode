139c852926d971cead18c28c874fdca278db3884
Saksham Aggarwal <saksham080305@gmail.com>
2026-06-28 23:14:07 +0530

    Add LeetCode/Medium/63-unique-paths-ii/try-5.cpp

---
+++ LeetCode/Medium/63-unique-paths-ii/try-5.cpp
@@ -0,0 +1,30 @@
+/*
+ * Problem #63: Unique Paths II
+ * Difficulty: Medium
+ * Submission: Try 5
+ * status: Accepted
+ * Language: cpp
+ * Date: 6/27/2026, 1:15:10 PM
+ * Link: https://leetcode.com/problems/unique-paths-ii/
+ */
+
+class Solution {
+public:
+    int rec(int r, int c, vector<vector<int>>& obstacleGrid, vector<vector<int>> &dp){
+        if(r < 0 || c < 0) return 0;
+        if(obstacleGrid[r][c] == 1) return 0;
+        if(r == 0 && c == 0) return 1;
+
+        if(dp[r][c] != -1) return dp[r][c];
+
+        int left = rec(r, c - 1, obstacleGrid, dp);
+        int up = rec(r - 1, c, obstacleGrid, dp);
+        return dp[r][c] = left + up;
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return rec(n - 1, m- 1, obstacleGrid, dp);
+    }
+};
