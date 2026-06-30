754f51d9c934769dc70bb3e5761ac3727c0b6d8d
Saksham Aggarwal <saksham080305@gmail.com>
2026-06-30 19:37:13 +0530

    Add LeetCode/Medium/494-target-sum/try-3.cpp

---
+++ LeetCode/Medium/494-target-sum/try-3.cpp
@@ -0,0 +1,46 @@
+/*
+ * Problem #494: Target Sum
+ * Difficulty: Medium
+ * Submission: Try 3
+ * status: Accepted
+ * Language: cpp
+ * Date: 6/28/2026, 7:47:02 PM
+ * Link: https://leetcode.com/problems/target-sum/
+ */
+
+class Solution {
+public:
+    int perfectSum(vector<int>& arr, int sum) {
+        // code here
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(sum+ 1, 0));
+        
+        for(int i = 0; i < n; i++){
+            dp[i][0] = 1;
+        }
+        if (arr[0] <= sum) {
+            dp[0][arr[0]] += 1;
+        }
+        
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j <= sum; j++){
+                int notake = dp[i - 1][j];
+                
+                int take = 0;
+                if(arr[i] <= j){
+                    take = dp[i - 1][j - arr[i]];
+                }
+                
+                dp[i][j] = take + notake;
+            }
+        }
+        
+        return dp[n - 1][sum];
+    }
+    int findTargetSumWays(vector<int>& arr, int diff) {
+        int total = accumulate(arr.begin(), arr.end(), 0);
+        if(diff > total || total + diff < 0) return 0;
+        if((total + diff) & 1) return 0;
+        return perfectSum(arr, (total + diff) / 2);
+    }
+};
