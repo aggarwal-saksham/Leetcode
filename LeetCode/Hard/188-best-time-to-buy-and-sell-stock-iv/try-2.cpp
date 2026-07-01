32868055a7eba4c865ef1586647adf095df42ed9
Saksham Aggarwal <saksham080305@gmail.com>
2026-07-01 11:04:00 +0530

    Add LeetCode/Hard/188-best-time-to-buy-and-sell-stock-iv/try-2.cpp

---
+++ LeetCode/Hard/188-best-time-to-buy-and-sell-stock-iv/try-2.cpp
@@ -0,0 +1,37 @@
+/*
+ * Problem #188: Best Time to Buy and Sell Stock IV
+ * Difficulty: Hard
+ * Submission: Try 2
+ * status: Accepted
+ * Language: cpp
+ * Date: 6/30/2026, 8:03:57 PM
+ * Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
+ */
+
+class Solution {
+public:
+    int rec(int i, int k, vector<int>& prices, vector<vector<int>>& dp) {
+        if (i == prices.size())
+            return 0;
+        if (dp[i][k] != -1)
+            return dp[i][k];
+        if (k == 0) {
+            return 0;
+        } 
+        else if (k%2 == 0) {
+            int buy = -prices[i] + rec(i + 1, k - 1, prices, dp);
+            int leave = rec(i + 1, k, prices, dp);
+            return dp[i][k] = max(buy, leave);
+        } 
+        else {
+            int sell = prices[i] + rec(i + 1, k - 1, prices, dp);
+            int leave = rec(i + 1, k, prices, dp);
+            return dp[i][k] = max(sell, leave);
+        }
+    }
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> dp(n + 1, vector<int>(2 * k + 1, -1));
+        return rec(0, 2 * k, prices, dp);
+    }
+};
