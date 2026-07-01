0a12eea5c16d6d5737366dbe1c3785e1ba1b5518
Saksham Aggarwal <saksham080305@gmail.com>
2026-07-01 11:03:38 +0530

    Add LeetCode/Hard/1463-cherry-pickup-ii/try-1.cpp

---
+++ LeetCode/Hard/1463-cherry-pickup-ii/try-1.cpp
@@ -0,0 +1,39 @@
+/*
+ * Problem #1463: Cherry Pickup II
+ * Difficulty: Hard
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 6/28/2026, 12:57:08 AM
+ * Link: https://leetcode.com/problems/cherry-pickup-ii/
+ */
+
+class Solution {
+public:
+    int rec(int i, int j1, int j2, vector<vector<int>>& grid, vector<vector<vector<int>>> &dp){
+        int n =grid.size();
+        int m = grid[0].size();
+        if(j1 < 0 || j1 >= m || j2 < 0 || j2 >= m) return -1e9;
+        if(dp[i][j1][j2] != -1) return dp[i][j1][j2];
+
+        if(i == n - 1){
+            if(j1 == j2) return grid[i][j1];
+            else return grid[i][j1] + grid[i][j2];
+        }
+        int maxi = -1e9;
+        for(int d1 = -1; d1 <= 1; d1++){
+            for(int d2 = -1; d2 <= 1; d2++){
+                if(j1 == j2)
+                    maxi = max(maxi, grid[i][j1] + rec(i + 1, j1 + d1, j2 + d2, grid, dp));
+                else maxi = max(maxi, grid[i][j1] + grid[i][j2] + rec(i + 1, j1 + d1, j2 + d2, grid, dp));
+            }
+        }
+        return dp[i][j1][j2] = maxi;
+    }
+    int cherryPickup(vector<vector<int>>& grid) {
+        int n =grid.size();
+        int m = grid[0].size();
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, -1)));
+        return rec(0, 0, m - 1, grid, dp);
+    }
+};
