81a7c68a75218895dd402ccca87657a1dcf7780d
Saksham Aggarwal <saksham080305@gmail.com>
2026-07-01 11:03:32 +0530

    Update LeetCode/Medium/322-coin-change/try-2.cpp

--- LeetCode/Medium/322-coin-change/try-2.cpp
+++ LeetCode/Medium/322-coin-change/try-2.cpp
@@ -4,7 +4,7 @@
  * Submission: Try 2
  * status: Accepted
  * Language: cpp
- * Date: 6/28/2026, 11:24:51 PM
+ * Date: 6/28/2026, 9:05:53 PM
  * Link: https://leetcode.com/problems/coin-change/
  */
 
@@ -20,22 +20,6 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
         vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
-        for(int i = 0; i < n; i++){
-            dp[i][0] = 0;
-        }
-        for(int j = 1; j <= amount; j++){
-            if(j % coins[0] == 0) dp[0][j] = j / coins[0];
-            else dp[0][j] = 1e9;
-        }
-
-        for(int i = 1; i < n; i++){
-            for(int j = 1; j <= amount ;j++){
-                int take = 1e9;
-                if(coins[i] <= j) take = 1 + dp[i][j - coins[i]];
-                int notake = dp[i - 1][j];
-                dp[i][j] = min(take, notake);
-            }
-        }
-        return dp[n - 1][amount] >= 1e9 ? -1 : dp[n - 1][amount];
+        return rec(n - 1, amount, coins, dp) >= 1e9 ? -1 : rec(n - 1, amount, coins, dp);
     }
 };
