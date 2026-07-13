a7440b3e44c2aab261abc60a891480c172ee1a00
Saksham Aggarwal <saksham080305@gmail.com>
2026-07-13 14:21:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Children Sum in a Binary Tree/children-sum-in-a-binary-tree.cpp
+++ Difficulty: Medium/Children Sum in a Binary Tree/children-sum-in-a-binary-tree.cpp
@@ -1,36 +1,17 @@
-/*
-
-class Node {
-public:
-    int data;
-    Node* left;
-    Node* right;
-
-    Node(int data) {
-        data = data;
-        left = nullptr;
-        right = nullptr;
-    }
-};
-
-*/
 
 class Solution {
-    int check(Node* root){
-        if(!root->left && !root->right) return root->data;
-        
-        int l = (root->left) ? check(root->left) : 0;
-        int r = (root->right) ? check(root->right) : 0;
-        
-        if(l + r != root->data) return -1;
-        
-        if(l == -1 || r == -1) return -1;
-        
-        else return root->data;
-    }
   public:
+    int rec(Node *root){
+        if(!root) return 0;
+        if(!root->left && !root->right) return root->data;
+        int left = rec(root->left);
+        int right = rec(root->right);
+        if(left == -1 || right == -1) return -1;
+        if(left + right != root->data) return -1;
+        return root->data;
+    }
     bool isSumProperty(Node *root) {
         // code here
-        return check(root) != -1;
+        return rec(root) != -1;
     }
 };
