89a832517b0a2d1edd70cbc9006869427c3ceeed
Saksham Aggarwal <saksham080305@gmail.com>
2026-07-25 14:40:28 +0530

    Add LeetCode/Medium/3913-sort-vowels-by-frequency/try-1.cpp

---
+++ LeetCode/Medium/3913-sort-vowels-by-frequency/try-1.cpp
@@ -0,0 +1,49 @@
+/*
+ * Problem #3913: Sort Vowels by Frequency
+ * Difficulty: Medium
+ * Submission: Try 1
+ * status: Accepted
+ * Language: cpp
+ * Date: 7/25/2026, 3:31:52 AM
+ * Link: https://leetcode.com/problems/sort-vowels-by-frequency/
+ */
+
+class Solution {
+public:
+    string sortVowels(string s) {
+        unordered_map <int, int> freq;
+        int n = s.size();
+        vector<int> first(26, -1);
+        for(int i = n - 1; i >= 0; i--){
+            char c = s[i];
+            if(c == 'a' || c == 'e' ||c == 'i' || c == 'o' || c == 'u'){
+                freq[c]++;
+                first[c - 'a'] = i;
+            }
+            
+        }
+        priority_queue<tuple<int, int, int>> maxH;
+        for(auto i : freq){
+            int f = i.second;
+            int c = i.first;
+            maxH.push({f, -first[c - 'a'], c});
+        }
+        int i = 0;
+        while(i < n){
+            while(!maxH.empty()){
+                auto[f, _, nc] = maxH.top();
+                maxH.pop();
+                while(i < n && f > 0){
+                    char c = s[i];
+                    if(c == 'a' || c == 'e' ||c == 'i' || c == 'o' || c == 'u'){
+                        s[i] = nc; 
+                        f--;
+                    }
+                    i++;
+                }
+            }
+            i++;
+        }
+        return s;
+    }
+};
